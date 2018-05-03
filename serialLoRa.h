0e3b9ded9ee22c727cf795fd3e2d6f0c63e6206c
Antonin Héréson <antonin.hereson@smile.fr>
2018-05-03 18:40:04 +0200

    add header

--- serialLoRa.h
+++ serialLoRa.h
@@ -4,8 +4,10 @@
  * http://pficheux.free.fr/articles/lmf/serial/
  * https://en.wikibooks.org/wiki/Serial_Programming/termios
  */
-
-int serialExchange(char *port, char *dataIN, size_t sizeDataIN, char *dataOUT, size_t sizeDataOUT){
+#ifndef SERIAL_LORA_H
+#define SERIAL_LORA_H
 
 #include <unistd.h>
+int serialExchange(char *port, char *dataIN, size_t sizeDataIN, char *dataOUT, size_t sizeDataOUT){
 
+#endif
