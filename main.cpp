796171a500b62136532e906602c392532e224486
Antonin Héréson <antonin.hereson@smile.fr>
2018-07-27 17:51:52 +0200

    Change Serial lora to a class

--- main.cpp
+++ main.cpp
@@ -2,15 +2,16 @@
 #include "forwarder.hpp"
 #include "serial_lora.hpp"
 
-#include <thread> // std::thread
 #include <iostream> // sdt::cout, sdt::cin, sdt::endl
+#include <thread>   // std::thread
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
 
-void thread_Cpu_data() {
+void thread_Cpu_data(SerialLora &my_serial_port) {
+    cout << "thread cpu started" << endl;
     const auto size_buffer = 200;
     while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
@@ -20,13 +21,14 @@ void thread_Cpu_data() {
             my_buff.msg[my_buff.size] = e;
             my_buff.size++;
         }
-        write_serial_Lora(my_buff);
+        my_serial_port.write_serial_Lora(my_buff);
     }
 }
 
-void thread_consummer() {
+void thread_consummer(SerialLora &my_serial_port) {
+    cout << "thread consumer started" << endl;
     while (true) {
-        auto my_buffer = read_serial_Lora();
+        auto my_buffer = my_serial_port.read_serial_Lora();
         forwarder(my_buffer.msg.get(), my_buffer.size);
     }
 }
@@ -40,14 +42,15 @@ int main(int argc, char *argv[]) {
 
     // init packet forwarder
     parseCommandline(argc - 1, argv + 1);
+    auto my_port = std::string(argv[1]);
 
-    std::thread t1(thread_consummer);
-    std::thread t2(thread_Cpu_data);
-    std::thread t3(serial_exchange, argv[1], 200);
+    SerialLora my_serial_port(my_port, 200);
+
+    std::thread t1(thread_consummer, std::ref(my_serial_port));
+    std::thread t2(thread_Cpu_data, std::ref(my_serial_port));
 
     t1.join();
     t2.join();
-    t3.join();
 
     return 0;
 }
