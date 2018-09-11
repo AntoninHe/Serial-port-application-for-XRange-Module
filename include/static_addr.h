2ecff43274877722fe1551e8e8e63a76afef6088
Antonin Héréson <antonin.hereson@smile.fr>
2018-09-11 10:03:54 +0200

    Add Cmake version requirement in each Cmake file

---
+++ include/static_addr.h
@@ -0,0 +1,6 @@
+#define STATIC_MAC_ADDR_0 0x<yourAdress> ;
+#define STATIC_MAC_ADDR_1 0x<yourAdress> ;
+#define STATIC_MAC_ADDR_2 0x<yourAdress> ;
+#define STATIC_MAC_ADDR_3 0x<yourAdress> ;
+#define STATIC_MAC_ADDR_4 0x<yourAdress> ;
+#define STATIC_MAC_ADDR_5 0x<yourAdress> ;
